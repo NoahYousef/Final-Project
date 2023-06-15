a821258326cb99f4c7cd482096e968fa88fb8d4e
NoahYousef <noahy565@gmail.com>
2023-06-14 21:21:10 -0600

    boss

--- Scenes/Enemy/Boss.gd
+++ Scenes/Enemy/Boss.gd
@@ -1,21 +1,19 @@
 extends KinematicBody2D
 
 const BossDeathEffect = preload("res://Scenes/Enemy/BossDeathEffect.tscn")
-
+const LaserCharge = preload("res://Scenes/Enemy/Laser_Charge.tscn")
 
 var speed = 10
 var motion = Vector2.ZERO
 var player = null
 var character_dir = "idle"
-
+var target_rotation = null
 
 onready var attack_timer = $AttackTimer
 onready var anim = $AnimatedSprite
 onready var stats = $Enemy_Stats
 onready var hurtbox = $HurtBox
 onready var laser_spawn = $AnimatedSprite/Position2D
-var target_rotation = null
-
 
 
 func _ready():
@@ -25,7 +23,9 @@ func _ready():
 func _physics_process(delta):
 	
 	motion = Vector2.ZERO
-	if player:
+	if player == null:
+		return
+	elif player:
 		motion = position.direction_to(player.position) * speed
 		anim.play("idle")
 		if (player.position.x - position.x) < 0:
@@ -63,6 +63,10 @@ func _on_Enemy_Stats_no_health():
 func _on_AttackTimer_timeout():
 	if player == null:
 		return
+	shoot_laser()
+	
+
+func shoot_laser():
 	var laser = preload("res://Scenes/Enemy/Laser.tscn").instance()
 	add_child(laser)
 	print(laser)
@@ -70,8 +74,11 @@ func _on_AttackTimer_timeout():
 	laser.rotation = target_rotation
 
 
+
 func rotate_to_target():
 	var target_angle := PI / 2
 	if player:
 		target_angle = player.global_position.angle_to_point(global_position)
 		target_rotation = target_angle
+
+
